975bbbc2dfed9744114d3fa322e1fe7cd4791839
eliananana1 <eliana.cristinas2@hotmail.com>
2025-07-12 16:53:57 -0300

    redirecionamento e correções

--- login/login.js
+++ login/login.js
@@ -1,15 +1,17 @@
 const btnEntrar = document.getElementById('entrar');
 const email = document.getElementById('email');
-
+const senha = document.getElementById('senha');
 
 btnEntrar.onclick = () => {
     const usuario = email.value
     localStorage.setItem('email', usuario);
 
 
- btnEntrar.disabled = !(emailValido && senha.length > 5);
+ btnEntrar.disabled = !(email.length && senha.length > 5);
 
  console.log('Email salvo:', usuario);
-        alert('Login realizado com sucesso!');
+ window.alert('Login realizado com sucesso!');
+
+ window.location.href = 'listagemParceiros.html'
 
 }
